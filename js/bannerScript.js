d669338b8422a51aed8a0d2b5421aed49038e135
Rohith Krishna <roal2027@gmail.com>
2023-10-04 10:56:52 +0530

    banner

---
+++ js/bannerScript.js
@@ -0,0 +1,222 @@
+// // -----------------------------------------------------
+
+
+var selecteBannerid;
+var banner = []
+displaybanner()
+function displaybanner() {
+    fetch("http://localhost:5001/banner/getAllBanners")
+        .then(response => response.json())
+        .then(data => {
+            banner = data.
+                console.log(data);
+            setBanner()
+        })
+        .catch(error => {
+            console.error("the error is:", error);
+        });
+}
+function setBanner() {
+    banner.forEach(item => {
+        const row = document.createElement("tr");
+
+        const idCell = document.createElement("td");
+        idCell.textContent = banner.indexOf(item) + 1;
+        row.appendChild(idCell);
+
+        const titleCell = document.createElement("td");
+        titleCell.textContent = item.titleCell;
+        row.appendChild(titleCell);
+
+        const descriptionCell = document.createElement("td");
+        descriptionCell.textContent = item.bannerDescription;
+        row.appendChild(descriptionCell);
+
+        const imageCell = document.createElement("td");
+        imageCell.textContent = item.bannerImage;
+        row.appendChild(imageCell);
+
+        //EDIT BUTTON
+        const buttonCell = document.createElement("td");
+        const editButton = document.createElement("button");
+        editButton.classList.add("btn", "btn-success");
+        editButton.textContent = "EDIT";
+        editButton.addEventListener("click", function () {
+            selectedProductid = item._id
+            document.getElementById("bannername").value = item.bannerTitle
+            document.getElementById("bannerdescription ").value = item.bannerDescription
+            document.getElementById("bannerimage").value = item.bannerImage
+            console.log("BANNER UPDATE BUTTON clicked");
+            $('#myModal').modal('show')
+        });
+        buttonCell.appendChild(editButton);
+        row.appendChild(buttonCell);
+
+
+        // DELETE BUTTON
+        const buttCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn", "btn-danger");
+        deleteButton.textContent = "DELETE";
+        deleteButton.addEventListener("click", function () {
+            Swal.fire({
+                title: 'You Sure?',
+                text: "This banner will be permanently deleted !",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#1CC88A',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch("http://localhost:5001/banner/deleteBanner/" + item._id, {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            location.reload();
+                        })
+                        .catch(error => {
+                            console.error('Error:', error);
+                        });
+                    console.log("DELETE button  clicked!!");
+                }
+            });
+        });
+
+
+        buttCell.appendChild(deleteButton);
+        row.appendChild(buttCell);
+
+
+
+        tbody.appendChild(row);
+    })
+}
+
+// -----------------------------------------------------
+
+// create BANNER
+function createBanner() {
+    let body = {
+        bannerTitle: document.getElementById("bannername").value,
+        bannerDescription: document.getElementById("bannerdescription").value,
+        bannerImage: document.getElementById("bannerimage").value,
+    }
+    fetch("http://localhost:5001/banner/createBanner", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    })
+        .then(response => response.json())
+        .then(data => {
+            banner = data.data
+            console.log(data);
+            if (data.statusCode === 400) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: data.message
+                })
+            }
+            else {
+                location.reload()
+            }
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Sample data for banners (replace this with your actual data)
+
+
+// function setBanners() {
+//     const tbody = document.getElementById("banner-body");
+
+//     banners.forEach(banner => {
+//         const row = document.createElement("tr");
+
+//         const idCell = document.createElement("td");
+//         idCell.textContent = banner.id;
+//         row.appendChild(idCell);
+
+//         const imageCell = document.createElement("td");
+//         const imageElement = document.createElement("img");
+//         imageElement.src = banner.imageUrl;
+//         imageElement.alt = banner.altText;
+//         imageElement.className = "banner-image"; // Add CSS class for styling if needed
+//         imageCell.appendChild(imageElement);
+//         row.appendChild(imageCell);
+
+//         const altTextCell = document.createElement("td");
+//         altTextCell.textContent = banner.altText;
+//         row.appendChild(altTextCell);
+
+//         const editCell = document.createElement("td");
+
+//         row.appendChild(editCell);
+
+//         const deleteCell = document.createElement("td");
+//         // Add delete button or any other action button if needed
+//         // For example:
+//         // const deleteButton = document.createElement("button");
+//         // deleteButton.textContent = "Delete";
+//         // deleteButton.onclick = () => deleteBanner(banner.id);
+//         // deleteCell.appendChild(deleteButton);
+//         row.appendChild(deleteCell);
+
+//         tbody.appendChild(row);
+//     });
+// }
+
+// // Call the function to populate the table
+// setBanners();
