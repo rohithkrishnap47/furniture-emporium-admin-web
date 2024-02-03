9f7111a81940395e93b19a8e11a1799fdb402d64
Rohith Krishna <roal2027@gmail.com>
2024-02-04 04:24:17 +0530

    delivery status update button

--- js/orderScript.js
+++ js/orderScript.js
@@ -3,41 +3,41 @@ var selectuserid;
 var user = []
 displayuser()
 function displayuser() {
-    fetch("http://localhost:5001/user/listusers")
+    fetch("http://localhost:5001/user/show-orders")
         .then(response => response.json())
         .then(data => {
-            user = data.data
-            console.log("order", data.data);
-            setUser()
+            user = data
+            console.log(user);
+            showOrders()
         })
         .catch(error => {
             console.error("the error is:", error);
         });
 }
-function setUser() {
+function showOrders() {
     const tbody = document.getElementById("data-body");
 
-    user?.forEach(item => {
+    user.forEach((item, index) => {
         const row = document.createElement("tr");
 
         const idCell = document.createElement("td");
-        idCell.textContent = user.indexOf(item) + 1;
+        idCell.textContent = index + 1;
         row.appendChild(idCell);
 
         const productCell = document.createElement("td");
-        productCell.textContent = item.firstname;
+        productCell.textContent = item._id;
         row.appendChild(productCell);
 
         const payMtdCell = document.createElement("td");
-        payMtdCell.textContent = item.emailaddress;
+        payMtdCell.textContent = item.paymentMethod;
         row.appendChild(payMtdCell);
 
         const statusCell = document.createElement("td");
-        statusCell.textContent = item.userStatus;
+        statusCell.textContent = item.paymentStatus;
         row.appendChild(statusCell);
 
         const orderDate = document.createElement("td");
-        orderDate.textContent = item.userStatus; // Placeholder value, replace with actual order date
+        orderDate.textContent = item.orderedDate; // Placeholder value, replace with actual order date
         row.appendChild(orderDate);
 
         const deliveryStatus = document.createElement("td");
@@ -51,19 +51,24 @@ function setUser() {
         deliveryOptions.forEach(optionText => {
             const option = document.createElement("option");
             option.textContent = optionText;
-            option.value = optionText.toLowerCase(); // set the value to lowercase of option text
+            option.value = optionText; // set the value to lowercase of option text
             deliveryDropdown.appendChild(option);
         });
-        // Add event listener to dropdown
+        deliveryDropdown.value = item.deliveryStatus
         deliveryDropdown.addEventListener("change", function () {
             // Get the selected value
             const selectedValue = this.value;
             // Here, you can perform any actions based on the selected value
             console.log("Selected delivery status:", selectedValue);
+            changeStatus(selectedValue, item._id)
         });
         // Append dropdown to cell
         deliveryStatus.appendChild(deliveryDropdown);
         row.appendChild(deliveryStatus);
+
+        const detail = document.createElement("td");
+        detail.textContent = "Detail"; // Placeholder value, replace with actual order date
+        row.appendChild(detail);
 
         // BLOCK BUTTON
         // const buttCell = document.createElement("td");
@@ -172,4 +177,27 @@ function createBanner() {
         .catch(error => {
             console.error('Error:', error);
         });
+}
+
+// ----------------------------------------------------------
+// UPDATE-ORDER-STATUS
+async function changeStatus(status, id) {
+    try {
+        const response = await fetch('http://localhost:5001/user/update-order-status/' + id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+
+            },
+            body: JSON.stringify({
+                deliveryStatus: status
+            })
+        });
+
+        const data = await response.json();
+
+        console.log('Updated status:', data.deliveryStatus);
+    } catch (error) {
+        console.error('Error updating status:', error);
+    }
 }
